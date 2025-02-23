9929f967ec77d0b7b194abca09de464ee4d40734
vikramk100 <122198352+vikramk100@users.noreply.github.com>
2025-02-23 00:00:42 -0500

    Create page.js for Scam Detection

---
+++ frontend/app/scam-detection/page.js
@@ -0,0 +1,7 @@
+'use client'; // Required since we're using `useState` inside App.jsx
+
+import ScamDetection from '@/components/ScamDetection';
+
+export default function DetectionPage() {
+    return <ScamDetection />;
+}
