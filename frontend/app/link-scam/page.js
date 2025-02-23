d5f103bb70c2d61b980ea7cd77ba6022e1849e23
vikramk100 <122198352+vikramk100@users.noreply.github.com>
2025-02-23 01:06:59 -0500

    Update page.js

--- frontend/app/link-scam/page.js
+++ frontend/app/link-scam/page.js
@@ -1,1 +1,7 @@
+'use client'; // Required since we're using `useState` inside App.jsx
 
+import LinkScam from '@/components/LinkScam';
+
+export default function DetectionPage() {
+    return <LinkScam />;
+}
